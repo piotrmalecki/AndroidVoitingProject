8f500a4fffdb3d7f7df3c4f463e68d111ac22275
Piotr Malecki <piotrr.malecki@gmail.com>
2013-10-06 12:31:51 +0200

    testing branches

--- VotingProject/src/com/example/votingproject/MainActivity.java
+++ VotingProject/src/com/example/votingproject/MainActivity.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
+	public static int example;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
